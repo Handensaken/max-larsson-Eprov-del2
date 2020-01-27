d7c8f8baeef5e0a95c32d8469c139048f0eee8ac
Handensaken <60339724+Handensaken@users.noreply.github.com>
2020-01-27 09:10:57 +0100

    Add project files.

---
+++ max-larsson-Eprov-del2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace max_larsson_Eprov_del2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //skapar arrayer för namn och deras priser
+            string[] names = { "bread", "jam", "milk", "bone hurting juice", "juice that makes you buy bone hurting juice" };
+            int[] weight = { 0, 0, 0, 0, 0 };
+            //skapar en array som är detsamma som listan Market()
+            var dicc = new Dictionary<string, int>();
+            dicc = Market(names, weight);
+            //skriver ut vad du orkar bära
+            Console.WriteLine("items you can potentially carry:");
+                Console.WriteLine(dicc["hje"]);
+            Console.WriteLine(dicc["hej"]);
+            Console.WriteLine(dicc["jhe"]);
+
+            Press();
+        }
+        static int Limit() //metod som tar in användarens input för hur mycket den ska kunna bära
+        {
+            int weightLimitInt = 0;
+            bool success = false;
+            while (!success)
+            {
+                Console.WriteLine("enter carry weight limit");
+                string weightLimitString = Console.ReadLine();
+                success = int.TryParse(weightLimitString, out weightLimitInt);
+                if (!success)
+                {
+                    Console.WriteLine("enter a number");
+                }
+                Press();
+            }
+            return weightLimitInt;
+        }
+        static int Market(string[] names, int[] weight)    //List metod som skriver ut vad sakerna heter samt deras pris
+        {
+            //sätter limiten till limitmetoden
+            int weightLimit = Limit();
+            List<string> inventory = new List<string>();
+            for (int i = 0; i < weight.Length; i++)
+            {
+                //slumpar vikten på de olika föremålen
+                weight[i] = rand.Next(1, 101);
+                //lägger till namnet på de objekt rollpersonen kan bära i listan som returneras
+                if (weight[i] <= weightLimit)
+                {
+                    inventory.Add(names[i]);
+                }
+                Console.WriteLine(names[i] + ": " + weight[i]);
+
+                Press();
+            }
+            Press();
+            var dict = new Dictionary<string, int>
+            {
+                {"hje", 1 },
+                {"hej", 2 },
+                {"jhe", 3 }
+            };
+            return dict["hje"];
+        }
+        static void Press() //metod som gör det lite tydligare vad spelaren ska göra när ett knapptryck förväntas
+        {
+            Console.WriteLine("press any key to continue");
+            Console.ReadKey(true);
+            Console.Clear();
+        }
+        static Random rand = new Random();
+    }
+}
